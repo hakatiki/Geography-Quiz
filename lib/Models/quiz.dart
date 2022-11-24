8c99e9f6167224d7fe099d61a66063e80484022b
ptnoemi <noemi.paptakacs@cloudera.com>
2022-11-24 14:41:26 +0100

    Quiz model and db provider

---
+++ lib/Models/quiz.dart
@@ -0,0 +1,8 @@
+
+class Quiz {
+  late String question;
+  late List<String> answers;
+
+  Quiz(this.question,
+  this.answers);
+}
---
+++ lib/Providers/db_provider.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
+import '../Models/http_exception.dart';
+import '../Models/quiz.dart';
+
+class DBProvider {
+  final String date = '05-11-22';
+  final String DBurl =
+      'https://geo-quiz-nb-default-rtdb.firebaseio.com/';
+
+
+  Future<Quiz> getQuiz() async {
+    final String url = DBurl+'/'+date;
+    final response = await http.get(Uri.parse(url));
+    final data = json.decode(response.body);
+    var quiz = Quiz(data['question'], data['answer']);
+    print(quiz);
+    return quiz;
+  }
+
+}
